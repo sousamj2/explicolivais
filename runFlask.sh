a28973532f5e6af5a3d4ac88863afac32fe91ba0
Mário LEGION <mdacunha@cern.ch>
2025-10-22 20:16:20 +0100

    Commented out some pages to test server.

---
+++ runFlask.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=application.py
+export FLASK_ENV=development
+flask run --host=localhost --port=8080
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from waitress import serve
 from application import app
-from connectDB import check_and_create_tables
+# from connectDB import check_and_create_tables
+from DBcreateTables import createAllTables
 
 if __name__ == '__main__':
-    check_and_create_tables()
+    # check_and_create_tables()
+    createAllTables()
     # For production use waitress to serve the app
 
     # Localhost access only
-    serve(app, host='localhost', port=8080)
+    serve(app, host='www.explicacoeslisboa.pt', port=80)
     # serve(app, host='0.0.0.0', port=8080)
